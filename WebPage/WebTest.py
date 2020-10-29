878469dc7259eeefae28633ece003e371213ce87
Mark The Magnificent <mnegretev@gmail.com>
2020-10-28 18:04:15 -0600

    Add some scripts for the web page

---
+++ WebPage/WebTest.py
@@ -0,0 +1,11 @@
+import rospy
+from geometry_msgs.msg import *
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, I am Marco!'
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
