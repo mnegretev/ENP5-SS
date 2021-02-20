16278c89c3456c56916f3ed70db56351bbb808e2
Ingrid_Serrano <ingrid.serrano.enp5.021@gmail.com>
2021-02-20 16:44:32 -0600

    Prueba 2 cvBridge

--- WebPage/main.py
+++ WebPage/main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, Response
 import cv2
 import rospy
 import threading
+import numpy as np
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -16,27 +17,29 @@ image_pub = rospy.Publisher("/camera/color/image_raw", Image, queue_size=1)
 
 camera = cv2.VideoCapture(-1) #webcamara
 bridge = CvBridge()
+
 def gen_frames(): 
     while True:
-        success, frame = camera.read()  # lee el marco de la cámara
-        cv_image = bridge.imgmsg_to_cv2(frame, desired_encoding='CV_8UC3')
+        success, frame = camera.read()  # lee el marco de la camara
+        #dtype, n_channels = bridge.encoding_as_cvtype2('8UC3')
+        #im = np.ndarray(shape=(480, 640, n_channels), dtype=dtype)
+        cv_image = bridge.imgmsg_to_cv2(frame, desired_encoding='8UC3')
 
         if not success:
             break
         else:
             ret, buffer = cv2.imencode('.jpg', cv_image)
-            #cv_image = buffer.tobytes()
+            cv_image = buffer.tobytes()
             yield (b'--cv_image\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + cv_image + b'\r\n') # concat frame uno por uno y muestra el resultado
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=cv_image')
 
 @app.route('/')
 def index():
     return render_template ('index.html')
 
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=cv_image')
 '''
 @app.route('/')
 def index():
